c9f4906d4f23440c934c8a250924fcfb02ce282a
AIGarifullin <volpatibon@ya.ru>
2024-06-01 06:42:37 +0000

    Add features to settigs.py file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,14 +1,27 @@
 from pathlib import Path
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['130.193.54.102', '127.0.0.1', 'localhost', 'voloyndxed.ddns.net']
 
+
+sentry_sdk.init(
+    # В этой переменной будет значение для вашего проекта.
+    dsn="https://72704c52296d1b50d4caac99408f0b6c@o4507345143005184.ingest.de.sentry.io/4507345161814096",
+    integrations=[
+        DjangoIntegration(),
+    ],
+    traces_sample_rate=1.0,
+    send_default_pii=True
+)
 
 # Application definition
 
@@ -103,7 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
